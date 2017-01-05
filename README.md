e957d9f9a4bc286a49b139a6f93c24b819b2920f
Quyen Tran <quyen.tran@s3corp.com.vn>
2017-01-05 15:55:52 +0700

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+api
+===
+
+A Symfony project created on December 12, 2016, 9:51 am.
---
+++ build.bat
@@ -0,0 +1,4 @@
+php app/console ca:cl --env=prod
+php app/console ca:cl --env=dev
+php app/console assets:install
+php app/console assetic:dump
