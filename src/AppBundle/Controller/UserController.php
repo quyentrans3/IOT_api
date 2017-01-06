49551acb2384c14e8314447a0f16595b1a259de9
Quyen Tran <quyen.tran@s3corp.com.vn>
2017-01-06 11:53:18 +0700

    debug

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -39,7 +39,8 @@ class UserController extends Controller
     	}catch(\Exception $ex){
 			$response->setData(array(
 				'status' => 500,
-				'message' => $ex->getMessage()
+				'message' => $ex->getMessage(),
+				'params' => $this->get('request')->request->all()
 			));
     	}
 		return $response;
